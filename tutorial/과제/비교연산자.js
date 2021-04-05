7b54f1b1a11a9a4007daaccfe3d732374f56b890
kzh4295 <kzh4295@naver.com>
2021-04-05 13:39:33 +0900

    비교연산자

---
+++ tutorial/과제/비교연산자.js
@@ -0,0 +1,7 @@
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // false(x) >> true (왜? 사전식비교)
+undefined == null //true
+undefined === null //false
+null == "\n0\n" //true(x) >>false(왜?  null은 오직 undefined와 같습니다.)
+null === +"\n0\n" //false
