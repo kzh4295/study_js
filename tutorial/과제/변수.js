588c7f314b89aff13b5f598c1a8bf76c6077a5a5
kzh4295 <kzh4295@naver.com>
2021-04-09 12:34:13 +0900

    ..

---
+++ tutorial/과제/변수.js
@@ -0,0 +1,33 @@
+// <!-- let admin
+// let name = "John"
+
+// name = admin
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <script> 
+//   alert(admin)
+//   </script>
+// </body>
+// </html> -->
+
+// =========================================
+//       정답 및 깨달음:
+
+// - 변수를 동시에 선언해도 되도 되는 구나
+// - 복사 받을 변수를 좌측에 배치하는 구나
+// =========================================
+
+let admin, name;
+name = "John";
+admin= name;
+alert(admin)
+
+
---
+++ tutorial/과제/상수.js
@@ -0,0 +1,2 @@
+const birthday = '18.04.1982';
+const  age = someCode(birthday);
